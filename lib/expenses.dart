117262e7e9ac46353ac69967796ce523aef046a9
Sushil Jacob <sushjacob3@gmail.com>
2026-03-24 12:16:31 -0400

    Made text and sized box

---
+++ lib/expenses.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget{
+  const Expenses({super.key});
+  
+  @override
+  State<StatefulWidget> createState() {
+    return _ExpensesState();
+  }
+
+}
+class _ExpensesState extends State<Expenses>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    body: Column(
+      children: [
+        Text('Chart goes here'),
+        SizedBox(height: 30),
+        Text('EXPENSE LIST GOES HERE'),
+      ],
+    ),
+    );
+  }
+  
+}
