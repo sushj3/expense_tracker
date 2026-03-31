185de35d2b71863e750a36912508c007e05aa2e1
Sushil Jacob <sushjacob3@gmail.com>
2026-03-31 11:31:26 -0400

    Added in starting card to expense_item and loaded into expenses list

---
+++ lib/widgets/expenses_list/expense_item.dart
@@ -0,0 +1,19 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class ExpenseItem extends StatelessWidget{
+  const ExpenseItem({super.key, required this.expense});
+  final Expense expense;
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 16,
+        ),
+        child: Text(expense.title),
+      ),
+    );
+  }
+}
