9d4d447b3f87568a24a95cecfc393526ce0c7917
Sushil Jacob <sushjacob3@gmail.com>
2026-04-08 15:13:32 -0400

    Added Date picker to models

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,15 +1,34 @@
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+final formatter = DateFormat.yMd();
 
 class NewExpense extends StatefulWidget{
   const NewExpense({super.key});
 
-State<NewExpense> createState(){
-  return _NewExpenseState();
+  State<NewExpense> createState(){
+    return _NewExpenseState();
   }
 }
 class _NewExpenseState extends State<NewExpense>{
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
+  DateTime? _selectedDate;
+
+  void _presentDatePicker() async {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year -1, now.month, now.day);
+    final pickedDate = await showDatePicker(
+      context:context,
+      initialDate: now,
+      firstDate: firstDate,
+      lastDate: now,  
+    );
+    print(pickedDate);
+    setState((){
+      _selectedDate = pickedDate;
+    });
+  }
 
   @override
   void dispose(){
@@ -17,41 +36,66 @@ class _NewExpenseState extends State<NewExpense>{
     _amountController.dispose();
     super.dispose();
   }
-
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(16),
       child: Column(
-      children:[
-        TextField(
-          controller: _titleController,
+        children:[
+          TextField(
+            controller: _titleController,
             maxLength: 50,
             keyboardType: TextInputType.name,
             decoration: InputDecoration(
               label: Text("Title"),
-            ), //InputDecoration
-        ), //TextField
-       
-          TextField(
-            controller:_amountController,
-            maxLength: 10,
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              prefixText: '\$',
-              label: Text('Amount'),
             ),
           ),
-        Row(children: [
-          ElevatedButton(onPressed: (){
-          }, child: Text("Cancel")), 
-          ElevatedButton(onPressed: (){
-            print(_titleController.text);
-            print(_amountController.text);
-          }, child: Text("Save Expense")) //ElevatedButton
-        ],)
-       ] 
-    ) //Column
-    ); //Padding
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller:_amountController,
+                  maxLength: 10,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    prefixText: '\$',
+                    label: Text("Amount"),
+                  ),
+                ),
+              ),
+              SizedBox(width:16),
+              Expanded(child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                Text(
+                  _selectedDate == null ?
+                  'Select Date' :
+                  formatter.format(_selectedDate!)
+                ),
+                IconButton(onPressed: _presentDatePicker, icon: const Icon(Icons.calendar_month))
+              ],),),
+            ],
+          ),
+          Row(children: [
+            DropdownButton(
+              items: Category.values.map(
+                (category) => DropdownMenuItem(
+                  value: category,
+                  child: Text(category.name.toString(),),
+                ),
+              ).toList(),
+              onChanged: (value){}),
+            ElevatedButton(onPressed: (){
+              Navigator.pop(context);
+            }, child: Text("Cancel")),
+            ElevatedButton(onPressed: (){
+              print(_titleController.text);
+              print(_amountController.text);
+            }, child: Text("Save Expense")),
+          ],)
+        ]
+      )
+  );
   }
 }
