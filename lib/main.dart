065de8603fab76b8fa1412fad7d4ce4763540412
Sushil Jacob <sushjacob3@gmail.com>
2026-04-16 11:32:33 -0400

    Finished Dark Mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,52 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
+ColorScheme kColorScheme = ColorScheme.fromSeed(
+  seedColor: Color.fromARGB(255, 96, 59, 181)
+  );
+  var kDarkColorScheme =ColorScheme.fromSeed(brightness: Brightness.dark, 
+  seedColor: const Color.fromARGB(255, 5, 99, 125),);
 void main() {
-  runApp(const MaterialApp(
+  runApp( MaterialApp(
+    theme: ThemeData(
+   textTheme:  ThemeData().textTheme.copyWith(
+      titleLarge: TextStyle(
+        fontWeight: FontWeight.normal,
+        color: kColorScheme.onSecondaryContainer,
+        fontSize: 20,
+      ),
+    ),
+
+      colorScheme: kColorScheme,
+     appBarTheme: AppBarTheme(
+      backgroundColor: kColorScheme.onPrimaryContainer,
+    foregroundColor: kColorScheme.primaryContainer,
+    ),
+    cardTheme: CardThemeData(
+      color: kColorScheme.secondaryContainer,
+      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: kColorScheme.primaryContainer,
+        ),
+    ),
+    ),
+    darkTheme: ThemeData.dark().copyWith(
+      colorScheme: kColorScheme,
+    cardTheme: CardThemeData(
+      color: kDarkColorScheme.secondaryContainer,
+      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: kDarkColorScheme.onPrimaryContainer,
+        foregroundColor: kDarkColorScheme.primaryContainer,
+        ),
+    ),
+    ),
+      themeMode: ThemeMode.system,
     home: Expenses()
-  ));
+  ),
+  );
 }
